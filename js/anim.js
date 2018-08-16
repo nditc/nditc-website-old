638c7f6f18e5208219cace3715aba5a790786864
NirobNabil <thetwin11100@gmail.com>
2018-08-16 15:40:07 +0600

    moved index.html body stuff to pages/home.html

--- js/anim.js
+++ js/anim.js
@@ -1,12 +1,12 @@
 function updateStyles() {
-  document.querySelectorAll('.navbar ul li').forEach(function(elem) {
-	if(elem.className.indexOf('div1') != -1){ continue }
-    elem.className = '';
-  })
+	document.querySelectorAll('.navbar ul li').forEach(function (elem) {
+		if (elem.className.indexOf('div1') != -1) { continue }
+		elem.className = '';
+	})
 
-  let route = window.location.hash.substring(2);
-  let elem = document.getElementById(route + 'l');
-  elem.className = 'active';
+	let route = window.location.hash.substring(2);
+	let elem = document.getElementById(route + 'l');
+	elem.className = 'active';
 }
 
 window.addEventListener('load', updateStyles);
