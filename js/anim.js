fd1464a480a0ea149ea24133306aaf98cec601ed
Shantanu Rahman <rahmanshantanu69@gmail.com>
2018-08-16 15:24:19 +0600

    Make sure to use proper indentaion

--- js/anim.js
+++ js/anim.js
@@ -1,11 +1,11 @@
 function updateStyles() {
-  document.querySelectorAll('.navbar ul li').forEach(function(elem) {
-    elem.className = '';
-  })
-  
-  let route = window.location.hash.substring(2);
-  let elem = document.getElementById(route + 'l');
-  elem.className = 'active';
+    document.querySelectorAll('.navbar ul li').forEach(function(elem) {
+        elem.className = '';
+    })
+
+    let route = window.location.hash.substring(2);
+    let elem = document.getElementById(route + 'l');
+    elem.className = 'active';
 }
 
 window.addEventListener('load', updateStyles);
