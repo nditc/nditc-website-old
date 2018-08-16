5025347187eb40edb927021e450e03dd4457053e
Shantanu Rahman <rahmanshantanu69@gmail.com>
2018-08-16 14:11:16 +0600

    Added a `.editorconfig`, fixed some syntax in css and make a basic spa

---
+++ js/anim.js
@@ -0,0 +1,12 @@
+function updateStyles() {
+  document.querySelectorAll('.navbar ul li').forEach(function(elem) {
+    elem.className = '';
+  })
+  
+  let route = window.location.hash.substring(2);
+  let elem = document.getElementById(route + 'l');
+  elem.className = 'active';
+}
+
+window.addEventListener('load', updateStyles);
+window.addEventListener('hashchange', updateStyles);
