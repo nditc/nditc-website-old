880f7bf55a622b2059aefa416c2bd837357c91b8
NirobNabil <thetwin11100@gmail.com>
2018-09-14 21:09:03 +0600

    added ui for techies page and deleted some unnecessary stuff from root directory

--- js/router.js
+++ js/router.js
@@ -1,24 +1,24 @@
 const app = document.getElementById('app');
+
 const routes = {
     'home': ['pages/home.html', ['css/home.css'], ['js/home.js'] ],
     'photos': ['pages/photos.html', ['css/photos.css'], ['js/photos.js'] ],
-    'techies': ['pages/techies/index.html', ['css/techies.css'], ['js/techies.js'] ],
+    'techies': ['pages/techies/index.html', ['css/techies.css'], ['js/techies.js',
+																  'https://www.gstatic.com/firebasejs/5.3.0/firebase-app.js',
+																  'https://www.gstatic.com/firebasejs/5.3.0/firebase-firestore.js',
+																  'pages/techies/firebase_base.js',
+																  'pages/techies/cfs.js',
+																  'pages/techies/minorUX.js',
+																  'pages/techies/md5.min.js'] ],
     'about': ['pages/about.html', ['css/about.css'], ['js/about.js'] ],
 	'init': ['pages/init.html', ['css/init.css'], ['js/init.js'] ]
 };
 
-var lastPageName = "";
+
+
+let lastPageName = "placeholder";			//this is a global
 function applyStyleAndScripts(pageName){
-	var head = document.getElementsByTagName("head")[0];
-	
-	//deletes every node in head that has its data-page set to lastPageName so that stylesheets and scripts of the last page is removed
-	document.getElementsByTagName('head')[0].childNodes.forEach(function(elem){
-		if(elem.dataset){
-			if(elem.dataset.page == lastPageName){
-				head.removeChild(elem);
-			}	
-		} 
-	})
+	let head = document.getElementsByTagName("head")[0];
 	
 	//loads every stylesheet of the specified page using the links stored in the second index of the routes dictionary
 	routes[pageName][1].forEach(function(route){
@@ -27,7 +27,7 @@ function applyStyleAndScripts(pageName){
 		StylesheetNode.type = 'text/css';
 		StylesheetNode.rel = 'stylesheet';
 		StylesheetNode.href = route;
-		StylesheetNode.media = 'screen';
+		//StylesheetNode.media = 'screen';
 		head.appendChild(StylesheetNode);
 	})
 	
@@ -40,6 +40,20 @@ function applyStyleAndScripts(pageName){
 		head.appendChild(ScriptNode);
 	})
 	
+	//deletes every node in head that has its data-page set to lastPageName so that stylesheets and scripts of the last page is removed
+	document.getElementsByTagName('head')[0].childNodes.forEach(function(elem){
+		if(elem.dataset){
+			if(elem.dataset.page == lastPageName){
+				head.removeChild(elem);
+			}	
+		} 
+	})
+	
+	window.scrollTo({
+		top: 0,
+		//behavior: "smooth"
+	});
+	
 	lastPageName = pageName;
 }
 
@@ -47,16 +61,17 @@ function applyStyleAndScripts(pageName){
 function getPage() {
     let pageName = window.location.hash.substring(2);   //store the target page name in var route
     let uri = routes[pageName][0];						//fetch the taget page uri from the 'routes' dicrtionary
-
+	
+	applyStyleAndScripts(pageName);	
+	
     fetch(uri)
         .then(function(response) {
             return response.text();
         })
         .then(function(text) {
             app.innerHTML = text;
-        });
+        })
 	
-	applyStyleAndScripts(pageName);
 }
 
 window.addEventListener('hashchange', getPage);
