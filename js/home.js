80db2c1cccf495368584912b2bb649d3cd15861e
Shantanu Rahman <rahmanshantanu69@gmail.com>
2018-09-21 23:01:37 +0600

    Login/logout looks ok

--- js/home.js
+++ js/home.js
@@ -6,17 +6,48 @@ document.getElementById('loginl').addEventListener('click', () => {
     glassfield.style.display = 'flex';
 });
 
+document.getElementById('logoutl').addEventListener('click', () => {
+    firebase.auth().signOut().then(() => {
+        console.log('Success!')
+    })
+});
+
 document.getElementById('loginclose').addEventListener('click', () => {
     loginbox.style.display = 'none';
     glassfield.style.display = 'none';
 });
 
 document.querySelectorAll('.navbar li').forEach((elem) => {
-    console.log(elem);
     elem.addEventListener('click', () => {
         document.querySelectorAll('.navbar li').forEach((elem) => {
             elem.classList.remove('active');
         });
         elem.classList.add('active');
     });
+});
+
+document.getElementById('login-submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    let email = document.getElementById('login-email').value;
+    let password = document.getElementById('login-password').value;
+    firebase.auth().signInWithEmailAndPassword(email, password).finally(() => {
+        loginbox.style.display = 'none';
+        glassfield.style.display = 'none';
+        console.log('Signed in!');
+    }).catch((err) => {
+        document.getElementById('pass-error').innerHTML = err.message;
+    })
+});
+
+let loginbtn = document.getElementById('loginl')
+let logoutbtn = document.getElementById('logoutl')
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        loginbtn.style.display = 'none';
+        logoutbtn.style.display = 'block';
+    } else {
+        loginbtn.style.display = 'block';
+        logoutbtn.style.display = 'none';
+    }
 });
